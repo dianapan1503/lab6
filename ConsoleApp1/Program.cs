dc8b3999171f00a7925ef42199b0c70ec6e837b1
dianapan1503 <D.Panasenko.FIT.121.20@knute.edu>
2021-11-21 16:51:26 +0200

    first commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Unicorn> unicorns = new List<Unicorn>();
+            unicorns.Add(new Unicorn { Name = "Bob", Health = 10, Agility = 7, Strength = 7 });
+            unicorns.Add(new Unicorn { Name = "Rob", Health = 6, Agility = 9, Strength = 8 });
+            foreach (Unicorn infou in unicorns)
+            {
+                infou.InfoMeth();
+            }
+            Console.Write("\n");
+            foreach (Unicorn unicornall in unicorns)
+            {
+                unicornall.Attack(10);
+                unicornall.Defend(2);
+                break;
+            }
+            Console.ReadKey();
+        }
+    }
+    interface ICharacter
+    {
+        string Name { get; set; }
+        double Health { get; set; }
+        double Agility { get; set; }
+        double Strength { get; set; }
+        void Attack(double distance);
+        void Defend(double damage);
+
+    }
+    class Unicorn : ICharacter
+    {
+        public string Name { get; set; }
+        public double Health { get; set; }
+        public double Agility { get; set; }
+        public double Strength { get; set; }
+        public void InfoMeth()
+        {
+            Console.WriteLine($"Ім'я єдинорога - {Name}");
+            Console.WriteLine($"Здоров'я - {Health}, Спритність -  {Agility}, Сила - {Strength}");
+        }
+        public void Attack(double distance)
+        {
+            Console.WriteLine($"Боб атакує Роба на дистанції {distance} метрів");
+        }
+        public void Defend(double damage)
+        {
+            Console.WriteLine($"Відбувається захист від атаки, {damage} удара");
+        }
+    }
+
+
+}
---
+++ ConsoleApp8/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                while (true)
+                {
+                    Console.WriteLine("Який формат бажаєте обрати?: ");
+                    string format = Console.ReadLine();
+                    if (format == "PNG")
+                    {
+                        PNG formatPNG = new PNG(format);
+                        formatPNG.BoxMeth();
+                    }
+                    else if (format == "DOC")
+                    {
+                        new DOC(format).BoxMeth();
+                    }
+                    else if (format == "JPEG")
+                    {
+                        new JPEG(format).BoxMeth();
+                    }
+                    else
+                    {
+                        Console.WriteLine("Помилка");
+                        
+                    }
+                    Console.ReadLine();
+                }
+
+            }
+        }
+        abstract class AbstractHandler
+        {
+            public abstract void Create();
+            public abstract void Open();
+            public abstract void Change();
+            public abstract void Save();
+        }
+        class AllMet : AbstractHandler
+        {
+            public string NameFormat { get; set; }
+            public AllMet(string name)
+            {
+                this.NameFormat = name;
+            }
+            public override void Create()
+            {
+                Console.WriteLine($"Creating {NameFormat} file");
+            }
+            public override void Open()
+            {
+                Console.WriteLine($"Opening {NameFormat} file");
+            }
+            public override void Change()
+            {
+                Console.WriteLine($"Changing {NameFormat} file");
+            }
+            public override void Save()
+            {
+                Console.WriteLine($"Saving {NameFormat} file");
+            }
+
+            public virtual void BoxMeth()
+            {
+                Create();
+                Open();
+                Change();
+                Save();
+            }
+
+        }
+        class PNG : AllMet
+        {
+            public PNG(string name)
+                : base(name)
+            { }
+            public override void BoxMeth()
+            {
+                base.BoxMeth();
+            }
+
+        }
+        class JPEG : AllMet
+        {
+            public JPEG(string name)
+                : base(name)
+            { }
+            public override void BoxMeth()
+            {
+                base.BoxMeth();
+            }
+
+        }
+        class DOC : AllMet
+        {
+            public DOC(string name)
+                : base(name)
+            { }
+            public override void BoxMeth()
+            {
+                base.BoxMeth();
+            }
+
+        }
+
+    }
